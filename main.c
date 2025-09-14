749f96c21a933b0238600d7276617bf2922bbe76
ADHISH V <adhishadhish547@gmail.com>
2025-09-14 13:50:23 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "encode.h"
+#include "decode.h"
+
+int main(int argc, char *argv[])
+{
+    OperationType op_type = check_operation_type(argv);
+    if(op_type == e_encode)
+    {
+        EncodeInfo encInfo;
+        printf("Encoding is selected :)\n");
+        if(read_and_validate_encode_args(argv,&encInfo)==e_success)
+        {
+            printf("Reading and validating arguments successful\n");
+            {
+                if(do_encoding(&encInfo)==e_success)
+                {
+                    printf("Encoding successful\n");
+                    printf("\n well done my boi :) !!\n");
+                }
+            }
+        }
+        else
+        printf("Error: Encoding failed\n");
+    }
+
+
+    else if(op_type==e_decode)
+    {
+        DecodeInfo decInfo;
+        printf("Decoding option is selected\n");
+        if(read_and_validate_decode_args(argv,&decInfo) == e_success)
+        {
+            printf("Reading and validating decode argument successful");
+            if(do_decoding(&decInfo)==e_success)
+            {
+                printf("Decoding successful\n");
+            }
+        }
+        else
+        printf("Sorry bro you file can't be decoded\n");
+    }
+    else if(op_type==e_help)
+    print_help();
+    else
+    printf("type -h to view help menu");
+}
